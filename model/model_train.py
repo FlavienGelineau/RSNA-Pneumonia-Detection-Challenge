f4cbf8e796a7416eff664da64e82685b8fb89b99
flavien.gelineau@gmail.com <ni4ooM7a>
2018-09-15 19:14:30 +0200

    histogram adaptative equalization

--- model/model_train.py
+++ model/model_train.py
@@ -2,28 +2,37 @@ import os
 
 import keras
 import tensorflow as tf
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler
 
 from data_processing.generator_definition import generator
 from data_processing.loading_data import load_filenames, load_pneumonia_locations
-from data_visualization.graphs import plot_graphs
+import numpy as np
 from model.cnn_segmentation import create_network, iou_bce_loss, mean_iou
-from model.making_predictions import make_submission
 
 
 def get_callbacks():
     early_stop = EarlyStopping(patience=10)
     checkpoint = ModelCheckpoint(filepath='weights_rsna.hdf5')
-    return [early_stop, checkpoint]
+
+    # cosine learning rate annealing
+    def cosine_annealing(x):
+        lr = 0.001
+        epochs = 20
+        return lr * (np.cos(np.pi * x / epochs) + 1.) / 2
+
+
+    learning_rate = LearningRateScheduler(cosine_annealing)
+
+    return [early_stop, checkpoint, learning_rate]
 
 
 if __name__ == '__main__':
     BATCH_SIZE = 16
     IMAGE_SIZE = 400
-    do_plot_graphs = True
+    do_plot_graphs = False
 
     folder_train = '../data/input/stage_1_train_images'
-    folder_test = '../input/stage_1_test_images'
+    folder_test = '../data/input/stage_1_test_images'
 
     test_filenames = os.listdir(folder_test)
 
@@ -34,13 +43,15 @@ if __name__ == '__main__':
 
     # create train and validation generators
 
-    train_filenames, valid_filenames = load_filenames(n_valid_samples=2500)
+    train_filenames, valid_filenames = load_filenames(n_valid_samples=2560)
     pneumonia_locations = load_pneumonia_locations()
 
     train_gen = generator(folder_train, train_filenames, pneumonia_locations, batch_size=BATCH_SIZE,
                           image_size=IMAGE_SIZE, shuffle=True, augment=True, predict=False)
     valid_gen = generator(folder_train, valid_filenames, pneumonia_locations, batch_size=BATCH_SIZE,
                           image_size=IMAGE_SIZE, shuffle=False, predict=False)
+
+
 
     keras.backend.get_session().run(tf.global_variables_initializer())
 
@@ -50,20 +61,3 @@ if __name__ == '__main__':
                                   epochs=30000,
                                   shuffle=True)
 
-    if do_plot_graphs:
-        plot_graphs(history)
-
-    # create test generator with predict flag set to True
-    test_gen = generator(folder_test,
-                         filenames=test_filenames,
-                         pneumonia_locations=None,
-                         batch_size=16,
-                         image_size=IMAGE_SIZE,
-                         shuffle=False,
-                         predict=True)
-
-    # create submission
-    make_submission(model,
-                    test_gen,
-                    test_filenames,
-                    submission_name='submission.csv')
