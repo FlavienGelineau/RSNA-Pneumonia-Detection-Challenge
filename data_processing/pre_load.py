2217871a1287205af43ad35dd6e1d9d2877d995d
Tristan Kalos <tristan.kalos@live.fr>
2018-09-16 02:32:47 +0200

    fixed preprocessing to work with DICOM images

--- data_processing/pre_load.py
+++ data_processing/pre_load.py
@@ -9,20 +9,26 @@ INPUT_TEST =  '../data/input/stage_1_test_images'
 OUTPUT_TRAIN = '../data/preprocessed_input/stage_1_train_images'
 OUTPUT_TEST = '../data/preprocessed_input/stage_1_test_images'
 
+
+def equalize(dicom):
+    img = dicom.pixel_array
+    print(img)
+    img = exposure.equalize_adapthist(img)*255
+    print(img.astype("uint8"))
+    dicom.PixelData = img.astype("uint8").tobytes()
+    #Dirty fix of a bug from pydicom guys
+    dicom[(0x7fe0,0x0010)].is_undefined_length = False
+    return dicom 
+
 if __name__ == '__main__':
     for img_name in os.listdir(INPUT_TRAIN):
         dicom = pydicom.dcmread(os.path.join(INPUT_TRAIN, img_name))
-        img = dicom.pixel_array
-        print(img.mean())
-        img = exposure.equalize_adapthist(img)
-        print(img.mean())
-        data = Image.fromarray(img*256)
-        data.convert("L").save(os.path.join(OUTPUT_TRAIN, img_name + ".bmp"))
+        dicom = equalize(dicom)
+        pydicom.filewriter.write_file(os.path.join(OUTPUT_TRAIN, img_name), dicom, write_like_original=True)
 
     for img_name in os.listdir(INPUT_TEST):
-        dicom = pydicom.dcmread(os.path.join(INPUT_TRAIN, img_name)).pixel_array
-        img = dicom.pixel_array
-        img = exposure.equalize_adapthist(img)
-        data = Image.fromarray(img*256)
-        data.convert("L").save(os.path.join(OUTPUT_TEST, img_name + ".bmp"))
+        dicom = pydicom.dcmread(os.path.join(INPUT_TEST, img_name))
+        dicom = equalize(dicom)
+        pydicom.filewriter.write_file(os.path.join(OUTPUT_TEST, img_name), dicom, write_like_original=True)
+
 
