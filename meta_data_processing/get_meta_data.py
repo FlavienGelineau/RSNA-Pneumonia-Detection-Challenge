b0d2d1f5ea075613d1520587761cf97c1c53fc50
Flavien Gelineau <you@example.com>
2018-09-25 00:11:59 +0200

    all metadata computed

--- meta_data_processing/get_meta_data.py
+++ meta_data_processing/get_meta_data.py
@@ -2,14 +2,16 @@ import gc
 import pandas as pd
 import pydicom
 import numpy as np
+import os
 
-
-def compute_metadata_file(df_meta, filename='train_metadata.csv'):
+def compute_metadata_file(df_meta, input_file = 'stage_1_train_images', filename='train_metadata.csv'):
+    file_names = os.listdir('../data/input/{}'.format(input_file))
+    file_names = [name for name in file_names if '.bmp' not in name]
     dcm_columns = None
-    for n, pid in enumerate(df_meta['patientId'].unique()):
+    for n, pid in enumerate(file_names):
         if n % 100 == 0:
-            print(n, len(df_meta['patientId'].unique().tolist()))
-        dcm_file = '../data/input/stage_1_train_images/%s.dcm' % pid
+            print(n, len(file_names))
+        dcm_file = '../data/input/{}/%s'.format(input_file) % pid
         dcm_data = pydicom.read_file(dcm_file)
 
         if not dcm_columns:
@@ -30,6 +32,41 @@ def compute_metadata_file(df_meta, filename='train_metadata.csv'):
     df_meta.to_csv(filename)
 
 
+def compute_metadata_test_file(input_file = 'stage_1_test_images', filename='test_metadata.csv'):
+    file_names = os.listdir('../data/input/{}'.format(input_file))
+    file_names = [name for name in file_names if '.bmp' not in name]
+    view_pos = []
+    age = []
+    sex = []
+    dcm_columns = None
+    test_metadata = pd.DataFrame()
+    test_metadata['filenames'] = file_names
+
+    for n, pid in enumerate(file_names):
+        if n % 100 == 0:
+            print(n, len(file_names))
+        dcm_file = '../data/input/{}/%s'.format(input_file) % pid
+        dcm_data = pydicom.read_file(dcm_file)
+
+        if not dcm_columns:
+            dcm_columns = dcm_data.dir()
+            dcm_columns.remove('PixelSpacing')
+            dcm_columns.remove('PixelData')
+
+        view_pos.append(dcm_data.data_element('ViewPosition').value)
+        age.append(dcm_data.data_element('PatientAge').value)
+        sex.append(dcm_data.data_element('PatientSex').value)
+
+        del dcm_data
+
+    gc.collect()
+
+    test_metadata['ViewPosition'] = view_pos
+    test_metadata['PatientAge'] = age
+    test_metadata['PatientSex'] = sex
+    test_metadata.to_csv(filename)
+
+
 if __name__ == '__main__':
     detailed_class_info = pd.read_csv('../data/input/stage_1_detailed_class_info.csv')
     train_labels = pd.read_csv('../data/input/stage_1_train_labels.csv')
@@ -37,3 +74,5 @@ if __name__ == '__main__':
 
     df_test_meta = pd.read_csv('../data/input/stage_1_detailed_class_info.csv')
 
+    #compute_metadata_file(df_test_meta, input_file='stage_1_test_images', filename='test_metadata.csv')
+    compute_metadata_test_file()
--- model/model_train.py
+++ model/model_train.py
@@ -13,10 +13,10 @@ from paths import INPUT_TRAIN, INPUT_TEST, OUTPUT_TRAIN, OUTPUT_TEST, INPUT_TRAI
 
 np.random.seed(42)
 
-def get_callbacks():
+def get_callbacks(img_size, batch_size):
     early_stop = EarlyStopping(patience=5)
 
-    filepath = "weights/weights-improvement-{epoch:02d}-{val_loss:.5f}.hdf5"
+    filepath = "weights/weights-{epoch:02d}-{val_loss:.5f}"+"-{}-{}.hdf5".format(img_size, batch_size)
     checkpoint = ModelCheckpoint(filepath, monitor='val_loss', save_best_only=False, verbose=1)
 
     # cosine learning rate annealing
@@ -32,8 +32,8 @@ def get_callbacks():
 
 
 if __name__ == '__main__':
-    BATCH_SIZE = 16
-    IMAGE_SIZE = 320
+    BATCH_SIZE = 9
+    IMAGE_SIZE = 512
     do_plot_graphs = False
 
     folder_train = INPUT_TRAIN_MODEL
@@ -41,8 +41,8 @@ if __name__ == '__main__':
 
     test_filenames = os.listdir(folder_test)
 
-    model = create_network(input_size=IMAGE_SIZE, channels=32, n_blocks=2, depth=4)
-    model.save_weights('weights/initial_weights.hdf5')
+    model = create_network(input_size=IMAGE_SIZE, channels=24, n_blocks=2, depth=4)
+    model.save_weights('weights/initial_weights_{}.hdf5'.format(IMAGE_SIZE))
     model.compile(optimizer='adam',
                   loss=iou_bce_loss,
                   metrics=['accuracy', mean_iou])
@@ -52,7 +52,7 @@ if __name__ == '__main__':
         print('model weights couldnt have been loaded')
 
     # create train and validation generators
-
+    print(model.summary())
     train_filenames, valid_filenames = load_filenames(n_valid_samples=2560, folder = folder_train)
     pneumonia_locations = load_pneumonia_locations()
 
